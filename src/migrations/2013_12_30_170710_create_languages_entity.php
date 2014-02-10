f2af74e3714e3b8bfe6290ceb53c266b85a147ab
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-02-10 08:51:18 +0100

    Update langs migration

--- src/migrations/2013_12_30_170710_create_languages_entity.php
+++ src/migrations/2013_12_30_170710_create_languages_entity.php
@@ -5,32 +5,71 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateLanguagesEntity extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('langs', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('code');
-			$table->string('i18n');
-			$table->boolean('is_active');
-			$table->boolean('is_default');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'langs',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('code');
+                $table->string('i18n');
+                $table->boolean('is_active');
+                $table->boolean('is_default');
+                $table->timestamps();
+            }
+        );
+
+        $langs = [
+            [
+                'code'       => 'en',
+                'i18n'       => 'en_UK',
+                'is_active'  => 1,
+                'is_default' => 1
+            ],
+            [
+                'code'       => 'pl',
+                'i18n'       => 'pl_PL',
+                'is_active'  => 1,
+                'is_default' => 0
+            ],
+            [
+                'code'       => 'de',
+                'i18n'       => 'de_DE',
+                'is_active'  => 1,
+                'is_default' => 0
+            ],
+            [
+                'code'       => 'ru',
+                'i18n'       => 'ru_RU',
+                'is_active'  => 1,
+                'is_default' => 0
+            ],
+            [
+                'code'       => 'fr',
+                'i18n'       => 'fr_FR',
+                'is_active'  => 0,
+                'is_default' => 0
+            ]
+        ];
+        foreach ($langs as $lang) { // Insert langs is mandatory
+            DB::table('langs')->insert($lang);
+        }
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('langs');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('langs');
+    }
 
 }
